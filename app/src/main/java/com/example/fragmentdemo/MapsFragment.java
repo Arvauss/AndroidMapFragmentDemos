50c40e0279cd271e544875cb8b1c2cb9b760eebe
Dewald-ST10119314 <ST10119314@vcconnect.edu.za>
2022-09-22 11:36:49 +0200

    ICE 5, On Map Click add polyline route

--- app/src/main/java/com/example/fragmentdemo/MapsFragment.java
+++ app/src/main/java/com/example/fragmentdemo/MapsFragment.java
@@ -4,21 +4,44 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.toolbox.HttpClientStack;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MapsFragment extends Fragment {
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        public GoogleMap mMap;
+        ArrayList markerPoints = new ArrayList();
 
         /**
          * Manipulates the map once available.
@@ -32,9 +55,167 @@ public class MapsFragment extends Fragment {
         public void onMapReady(GoogleMap googleMap) {
             LatLng curLatLng = new LatLng(MainActivity.lat, MainActivity.lng);
             googleMap.addMarker(new MarkerOptions().position(curLatLng).title("Current Location"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(curLatLng));;
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(curLatLng));
+            markerPoints.set(0, curLatLng);
+
+            mMap = googleMap;
+            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(@NonNull LatLng latLng) {
+                    if (markerPoints.size() > 1){
+                        markerPoints.remove(1);
+                        mMap.clear();
+                    }
+
+                    markerPoints.add(latLng);
+
+                    MarkerOptions options = new MarkerOptions();
+                    options.position(latLng);
+
+                    if (markerPoints.size() == 1) {
+                        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                    } else if (markerPoints.size() == 2) {
+                        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                    }
+                    mMap.addMarker(options);
+                    if (markerPoints.size() >= 2 ) {
+                        LatLng origin = (LatLng) markerPoints.get(0);
+                        LatLng dest = (LatLng) markerPoints.get(1);
+
+                        String url = getDirectionsURL(origin, dest);
+                        DownloadTask downloadTask = new DownloadTask();
+
+                        downloadTask.execute(url);
+                    }
+
+                }
+
+            });
         }
     };
+
+    private String getDirectionsURL(LatLng origin, LatLng dest) {
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+        String sensor = "sensor=false";
+        String mode = "mode+driving";
+
+        String parameters = str_origin + "&" + str_dest+ "&" + sensor + "&" + mode;
+
+        String output = "json";
+
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output +"?" + parameters;;
+        return url;
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data ="";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while((line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+        } catch (Exception e ){
+            Log.d("1234567", "Exception: " + e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    private class DownloadTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... url){
+            String data = "";
+            try {
+                data = downloadUrl(url[0]);
+            } catch (Exception e){
+                Log.d("12345678", "doInBackground: " + e.toString());
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            ParserTask parserTask = new ParserTask();
+
+            parserTask.execute(result);
+        }
+
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap>>>{
+
+        @Override
+        protected List<List<HashMap>> doInBackground(String... jsonData) {
+            JSONObject jObject;
+            List<List<HashMap>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = ne DirectionsJSONParser();
+
+                routes = parser.pase(jObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap>> result){
+            ArrayList points = null;
+
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            for( int i = 0; i < result.size(); i++){
+                points = new ArrayList();
+                lineOptions = new PolylineOptions();
+
+                List<HashMap> path = result.get(i);
+
+                for (int j = 0; j < path.size(); j++){
+                    HashMap point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat").toString());
+                    double lng = Double.parseDouble(point.get("lng").toString());
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                lineOptions.addAll(points);
+                lineOptions.width(12);
+                lineOptions.color(Color.RED);
+                lineOptions.geodesic(true);
+            }
+
+
+
+        }
+    }
 
     @Nullable
     @Override
